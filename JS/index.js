6dadf6c0c3284283f0fe0cc5a534299c8acdec8e
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-03-09 23:58:08 -0300

    new

--- JS/index.js
+++ JS/index.js
@@ -38,8 +38,16 @@ $(document).ready(function () {
     })
 
 
-
-
+//Effect appears text
+    $(".thiago").hover(function(){
+        $(".panel-display").toggle("2000",function(){
+            $(".thiago").animate({opacity: '0.5'})
+        })
+    })
+//popover
+    
+        $('[data-toggle="popover"]').popover();
+    
 
 });
 
