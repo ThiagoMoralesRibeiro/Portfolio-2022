64988ff283cebcfb27a894a119222707d1ec8817
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-03-16 11:17:23 -0300

    ib

--- JS/index.js
+++ JS/index.js
@@ -38,12 +38,11 @@ $(document).ready(function () {
     })
 
 
-//Effect appears text
-    $(".thiago").hover(function(){
-        $(".panel-display").toggle("2000",function(){
-            $(".thiago").animate({opacity: '0.5'})
-        })
+//Effect appears text change opacity
+$(".thiago").hover(function(){
+        $(".thiago").animate({opacity: '0.5'})
     })
+})
 //popover
     
         $('[data-toggle="popover"]').popover();
